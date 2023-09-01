db115c862ff65c2935f1c48065bb53a06e39e270
Josuad12 <joag5668@gmail.com>
2023-09-01 10:50:44 -0500

    cambio

---
+++ Parcial_Movil1.dart
@@ -0,0 +1,27 @@
+// Se desea realizar los siguientes criterios de aceptación de 1 requerimiento para el módulo de productos:
+// a.      Se debe tener un objeto de ventas donde se anexará cada producto que sea seleccionado
+// b.      Cada producto que estén 15 días a vencer se le otorgará un 15% de descuento
+// c.      Mostrar el total que se debe pagar
+
+
+
+class Prod{
+   String Nom_Producto;
+   double Precio_Producto;
+   DateTime Producto;
+
+   Prod(this.Nom_Producto, this.Precio_Producto, this.Producto)
+
+}
+
+CLass Venta{
+   List<Prod> productos = [];
+   void añadir_Prod(Prod prod) {
+      Prod.add(Prod);
+   }
+   }
+   Double TotalaPagar(){
+   TotalaPagar = 0;
+   }
+
+var 
